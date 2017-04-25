050d7816818e45b7e576de72536a64d07e8a21c8
Hector Mg <hectormgwake@gmail.com>
2017-04-25 15:38:58 -0500

    Extend available context specs

--- spec/apollo/bot/classifier_spec.rb
+++ spec/apollo/bot/classifier_spec.rb
@@ -13,11 +13,50 @@ describe Apollo::Bot::Classifier do
 
   context "when status is 'Available" do
     before { @classifier.status = "Available"}
-
     describe "#available?" do
       it "should return true" do
         expect(@classifier.available?).to be true
       end
     end
+    describe "#unavailable?"do
+      it "should return false" do
+        expect(@classifier.unavailable?).to be false
+      end
+    end
+    describe "#non_existent?"do
+      it "should return false" do
+        expect(@classifier.non_existent?).to be false
+      end
+    end
+    describe "#non_existent?"do
+      it "should return false" do
+        expect(@classifier.non_existent?).to be false
+      end
+    end
   end
+
+context "when status is 'Unavailable" do
+    before { @classifier.status = "Unavailable"}
+    describe "#available?" do
+      it "should return false" do
+        expect(@classifier.available?).to be false
+      end
+    end
+    describe "#unavailable?"do
+      it "should return true" do
+        expect(@classifier.unavailable?).to be true
+      end
+    end
+    describe "#non_existent?"do
+      it "should return false" do
+        expect(@classifier.unavailable?).to be false
+      end
+    end
+    describe "#non_existent?"do
+      it "should return false" do
+        expect(@classifier.unavailable?).to be false
+      end
+    end
+  end
+
 end
